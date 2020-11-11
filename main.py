c1cae30b54556bcace12c60e05829da9eb79f6c0
kebinr <kebinr@uninorte.edu.co>
2020-11-11 17:51:46 -0500

    configuracion completa

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uvicorn
+import gunicorn
 from typing import Optional
 from fastapi import Depends, FastAPI, Header, HTTPException
 from pydantic import BaseModel
@@ -15,19 +15,26 @@ from configurations.functions import *
 from models.places import *
 from models.sort import *
 from models.dictionaries import *
-from distribution import Colombia, States, Town, Neighborhood
+from distribution import *
+from distribution import Neigh
+from distribution import Colombia, States, Town
 from distribution.Colombia import datasets
 
 
 app = FastAPI()
 app.include_router(Colombia.router)
 app.include_router(States.router)
+app.include_router(Neigh.router)
 app.include_router(Town.router)
-app.include_router(Neighborhood.router)
+
+
+
+
+
+
 
 @app.get("/")
 def main():
     var = "This app is running..."
     return var
-
 
